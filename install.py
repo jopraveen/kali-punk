be858daab68aca7be354d579a8b8986d79bfc2b0
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-04-07 01:03:08 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,2 +1,2 @@
 # This is a one click install file it's still under testing, will update it soon :) 
-print("This is a one click install file it's still under testing, will update it soon"
+print("This is a one click install file it's still under testing, will update it soon")
